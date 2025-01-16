a49eca84e94c721f742616f9299aec7cf32bf761
Sebastian1657 <miler.sebastian1101@gmail.com>
2025-01-16 20:15:13 +0100

    Lista produktów,  małe zmiany w klasach

--- Projekt/Main.java
+++ Projekt/Main.java
@@ -15,6 +15,8 @@ class Kierowca {
 class Sklep{
     int x;
     int y;
+    int[] zapotrzebowanie;
+    int[][] inwentarz;
     public Sklep(int x, int y) {
         this.x = x;
         this.y = y;
@@ -26,12 +28,17 @@ class Sklep{
 class Magazyn{
     int x;
     int y;
+    int[][] inwentarz;
     public Magazyn(int x, int y) {
         this.x = x;
         this.y = y;
     };
+    public boolean isOnMagazyn(Kierowca kierowca) {
+        return kierowca.x == this.x && kierowca.y == this.y;
+    }
 }
 public class Main {
+    static Magazyn magazyn = new Magazyn(10*64,9*64);
     static Sklep sklep1 = new Sklep(64,4*64);
     static Sklep sklep2 = new Sklep(64,9*64);
     static Sklep sklep3 = new Sklep(6*64,8*64);
@@ -45,13 +52,13 @@ public class Main {
         int[][] droga = new int[][]{
                 {2*64,9*64,2*64}, {64,2*64,4*64}, {2*64,6*64,5*64},
                 {9*64,10*64,5*64}, {6*64,9*64,6*64}, {64,9*64,9*64},
-                {2*64,11*64,2*64}, {2*64,6*64,6*64}, {8*64,9*64,6*64},
-                {5*64,6*64,7*64}, {2*64,9*64,9*64}
+                {9*64,10*64,9*64},{2*64,11*64,2*64}, {2*64,6*64,6*64},
+                {8*64,9*64,6*64},{5*64,6*64,7*64}, {2*64,9*64,9*64}
         };
-        for(int i=0; i<6; i++){
+        for(int i=0; i<7; i++){
             if(kx >= droga[i][0] && kx <= droga[i][1] && ky == droga[i][2])return true;
         }
-        for(int i=6; i<11; i++){
+        for(int i=6; i<12; i++){
             if(ky >= droga[i][0] && ky <= droga[i][1] && kx == droga[i][2])return true;
         }
         return false;
@@ -62,6 +69,14 @@ public class Main {
         MainOkno.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         MainOkno.setSize(768, 798);
         MainOkno.setResizable(false);
+        JFrame MagazynOkno = new JFrame("Magazyn");
+        MagazynOkno.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        MagazynOkno.setSize(500,600);
+        MagazynOkno.setResizable(false);
+        JFrame SklepOkno = new JFrame("Sklep");
+        SklepOkno.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        SklepOkno.setSize(500,600);
+        SklepOkno.setResizable(false);
 
         Image mapa = new ImageIcon("Projekt/resources/map.png").getImage();
         Image pojazd = new ImageIcon("Projekt/resources/kierowcaTestowy.png").getImage();
@@ -73,6 +88,9 @@ public class Main {
                 g.drawImage(pojazd, gracz1.x, gracz1.y, this);
             }
         };
+        JPanel panelSklepZapotrzebowanie = new JPanel();
+        JPanel panelSklepInwentarz = new JPanel();
+        JPanel panelMagazyn = new JPanel();
         MainOkno.addKeyListener(
             new java.awt.event.KeyAdapter() {
             public void keyPressed(java.awt.event.KeyEvent e) {
@@ -102,12 +120,21 @@ public class Main {
                         }
                     }
                     case KeyEvent.VK_SPACE -> {
-                        Boolean onSklep = false;
+                        boolean onSklep = false;
+                        boolean onMagazyn = false;
                         for(Sklep sklep: sklepy){
                             if(sklep.isOnSklep(gracz1))onSklep = true;
                         }
+                        if (magazyn.isOnMagazyn(gracz1))onMagazyn = true;
                         if(onSklep){
                             System.out.println("Jesteś w sklepie");
+                            SklepOkno.setLocationRelativeTo(MainOkno);
+                            SklepOkno.setVisible(true);
+                        }
+                        if(onMagazyn){
+                            System.out.println("Jesteś w magazynie");
+                            MagazynOkno.setLocationRelativeTo(MainOkno);
+                            MagazynOkno.setVisible(true);
                         }
                     }
                 }
